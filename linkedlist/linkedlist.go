8ac9a59cb6d30ddd36455c03cc8c16c4607dbdba
Ram Goli <ramsgoli@gmail.com>
2019-09-10 23:00:14 -0700

    restructure api for linked list, introducing a linkedlist struct

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -9,39 +9,64 @@ type Node struct {
 	Key  interface{}
 }
 
-func LinkedList(key interface{}) *Node {
-	node := Node{nil, key}
-	return &node
+type LinkedList struct {
+	Size int
+	Head *Node
 }
 
-func (node *Node) Insert(key interface{}) {
-	pointer := node
+func New() *LinkedList {
+	return &LinkedList{0, nil}
+}
+
+func (l *LinkedList) Insert(key interface{}) {
+	if l.Head == nil {
+		l.Head = &Node{nil, key}
+		l.Size += 1
+		return
+	}
+
+	pointer := l.Head
 	for pointer.Next != nil {
 		pointer = pointer.Next
 	}
 
 	pointer.Next = &Node{nil, key}
+
+	l.Size += 1
 }
 
-func (node *Node) InsertNode(newNode *Node) {
-	pointer := node
+func (l *LinkedList) InsertNode(newNode *Node) {
+	if l.Head == nil {
+		l.Head = newNode
+		l.Size += 1
+		return
+	}
+
+	pointer := l.Head
 	for pointer.Next != nil {
 		pointer = pointer.Next
 	}
 
 	pointer.Next = newNode
+
+	l.Size += 1
 }
 
 // Remove removes a node with the specified value from the linked list
-func (node *Node) Remove(key interface{}) {
+func (l *LinkedList) Remove(key interface{}) {
+	if l.Head == nil {
+		return
+	}
+
 	// case 1. The head node is the node to remove
-	if node.Key == key {
-		node = node.Next
+	if l.Head.Key == key {
+		l.Head = l.Head.Next
+		l.Size -= 1
 		return
 	}
 	// case 2.
-	prev := node
-	curr := node.Next
+	prev := l.Head
+	curr := l.Head.Next
 
 	// iterate through list until we find key we want to remove
 	for curr != nil && curr.Key != key {
@@ -51,19 +76,19 @@ func (node *Node) Remove(key interface{}) {
 
 	if curr != nil {
 		prev.Next = curr.Next
+		l.Size -= 1
 	}
 }
 
-func (node *Node) Print() {
-	if node == nil {
+func (l *LinkedList) Print() {
+	if l.Head == nil {
 		return
 	}
 
-	if node.Next == nil {
-		fmt.Printf("%v\n", node.Key)
-		return
+	ptr := l.Head
+	for ptr.Next != nil {
+		fmt.Printf("%v -> ", ptr.Key)
+		ptr = ptr.Next
 	}
-
-	fmt.Printf("%v -> ", node.Key)
-	node.Next.Print()
+	fmt.Println(ptr.Key)
 }
